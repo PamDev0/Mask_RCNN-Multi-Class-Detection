fd40a4e3e65e01c1ce43f3711be587d9e5d49c53
Pâmela G <47401012+PamDev0@users.noreply.github.com>
2021-06-07 22:17:33 -0300

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2274,10 +2274,9 @@ class MaskRCNN():
         self.log_dir = os.path.join(self.model_dir, "{}{:%Y%m%dT%H%M}".format(
             self.config.NAME.lower(), now))
         
-        if self.mode == "training":
-	    # Create log_dir if not exists
-            if not os.path.exists(self.log_dir):
-		os.makedirs(self.log_dir)
+        # Create log_dir if not exists
+        if not os.path.exists(self.log_dir):
+            os.makedirs(self.log_dir)
 
         # Path to save after each epoch. Include placeholders that get filled by Keras.
         self.checkpoint_path = os.path.join(self.log_dir, "mask_rcnn_{}_*epoch*.h5".format(
