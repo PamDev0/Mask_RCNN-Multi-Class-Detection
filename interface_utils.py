8fd482d2670d8c5260ad297e19996a3469a2e408
Pâmela G <47401012+PamDev0@users.noreply.github.com>
2021-06-07 23:07:06 -0300

    Update interface_utils.py

--- interface_utils.py
+++ interface_utils.py
@@ -49,7 +49,7 @@ def detect_and_color_splash(model, image_path=None, video_path=None):
         # Run model detection and generate the color splash effect
         # print("Running on {}".format(image_path))
         # Read image
-        image = skimage.io.imread(image_path)
+        image = image = cv2.imread(image_path)
         # Detect objects
         r = model.detect([image], verbose=1)[0]
         # Color splash
@@ -115,7 +115,8 @@ def inference(image, weights):
         # print("Running on {}".format(image_path))
         # Read image
 
-        image = skimage.io.imread(image_path)
+        #image = skimage.io.imread(image_path)
+        image = cv2.imread(image_path)
 
         # Detect objects
         loaded_model = model.load_weights(model_path, by_name=True)
