5dc79650fa644c1b076051f2c7a72680aed7a479
Pâmela G <47401012+PamDev0@users.noreply.github.com>
2021-06-08 19:39:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpeg", "jpg
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     img_array = np.array(image)
-
+    st.write(img_array)
     #st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("Image Uploaded :) \nReady to predict!")
 
